948920388af2a2d07c6f1db1363b8a953c40dd99
samvela <samuelverso2708@gmail.com>
2025-03-07 18:43:12 -0600

    Se agregan los archivos de la carpeta templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,28 @@
-from flask import Flask
-#comentario de prueba
-#comentario para verificar que funciona el push en mi propia rama
-app = Flask(__name__)
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv 
+
+#Cargar las variables de entorno
+load_dotenv()
+
+#crear instancia
+app =  Flask(_name_)
+
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#importar modelos para SQLAlchemy los reconozca
+from app.models import Post
+
+#importar y registrar los blueprints
+from app.routes.post import posts_bp
+app.register_blueprint(posts_bp, url_prefix='/posts')
 
 @app.route('/')
-def hello_world():
-    return 'Hola Mundo'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def index()
+return "Hello World"
